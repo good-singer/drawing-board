9b375d7baa53e5056f1bbf9e2a42a6a4f8ff53da
good-singer <15732658216@163.com>
2019-11-05 15:25:25 +0800

    '兼容触屏设备'

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var context = canvas.getContext('2d')
 
 autoSetCanvasSize(canvas)
 
-listenToMouse(canvas)
+listenToUser(canvas)
 
 
 var eraserEnabled = false
@@ -35,41 +35,80 @@ function autoSetCanvasSize(canvas) {
   }
 }
 
-function listenToMouse(canvas) {
+function listenToUser(canvas) {
 
   var using = false
   var lastPoint = { x: undefined, y: undefined }
-  canvas.onmousedown = function (aaa) {
 
-    var x = aaa.clientX
-    var y = aaa.clientY
+  if (document.body.ontouchstart !== undefined) {
+    // 触屏设备
+    canvas.ontouchstart = function (aaa) {
+      console.log('kkkk')
+      var x = aaa.touches[0].clientX
+      var y = aaa.touches[0].clientY
+      console.log(x, y)
+      using = true
+      if (eraserEnabled) {
+        context.clearRect(x, y, 10, 10)
+      } else {
+        lastPoint = { "x": x, "y": y }
+      }
+    }
+    canvas.ontouchmove = function (aaa) {
+      console.log('jjjj')
+      var x = aaa.touches[0].clientX
+      var y = aaa.touches[0].clientY
 
-    using = true
-    if (eraserEnabled) {
-      context.clearRect(x, y, 10, 10)
-    } else {
-      lastPoint = { "x": x, "y": y }
+      if (!using) { return; }
+      if (eraserEnabled) {
+        console.log(1111)
+        context.clearRect(x, y, 10, 10)
+      } else {
+        var newPoint = { "x": x, "y": y }
+        // drawCircle(x, y, 1)
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
+      }
+    }
+    canvas.ontouchend = function () {
+      console.log('llll')
+      using = false
+    }
+  } else {
+    // 非触屏设备
+    canvas.onmousedown = function (aaa) {
+
+      var x = aaa.clientX
+      var y = aaa.clientY
+
+      using = true
+      if (eraserEnabled) {
+        context.clearRect(x, y, 10, 10)
+      } else {
+        lastPoint = { "x": x, "y": y }
+      }
+    }
+    canvas.onmousemove = function (aaa) {
+      var x = aaa.clientX
+      var y = aaa.clientY
+
+      if (!using) { return; }
+      if (eraserEnabled) {
+        console.log(1111)
+        context.clearRect(x, y, 10, 10)
+      } else {
+        var newPoint = { "x": x, "y": y }
+        // drawCircle(x, y, 1)
+        drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
+        lastPoint = newPoint
+      }
+    }
+    canvas.onmouseup = function (aaa) {
+      using = false
     }
   }
-  canvas.onmousemove = function (aaa) {
-    var x = aaa.clientX
-    var y = aaa.clientY
 
-    if (!using) { return; }
-    if (eraserEnabled) {
-      console.log(1111)
-      context.clearRect(x, y, 10, 10)
-    } else {
-      var newPoint = { "x": x, "y": y }
-      // drawCircle(x, y, 1)
-      drawLine(lastPoint.x, lastPoint.y, newPoint.x, newPoint.y)
-      lastPoint = newPoint
 
-    }
-  }
-  canvas.onmouseup = function (aaa) {
-    using = false
-  }
 
   function drawLine(x1, y1, x2, y2) {
     context.beginPath()
